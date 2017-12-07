70047c3ddd5020cecf34a3c28fa718b7660b243a
ryu2020 <ryu2020@pingry.org>
2017-12-07 02:01:12 -0500

    first commit!

--- src/Seven.java
+++ src/Seven.java
@@ -3,6 +3,41 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+class TreeNode{
+    String name;
+    int value;
+    ArrayList<TreeNode> pointers;
+    TreeNode(String s, int v){
+        name = s;
+        value = v;
+        pointers = new ArrayList<TreeNode>();
+    }
+    TreeNode(String s, int v, ArrayList<TreeNode> arrl){
+        name = s;
+        value = v;
+        pointers = arrl;
+    }
+    public int getValue(){
+        return value;
+    }
+
+    public boolean isEnd(){
+        return pointers.isEmpty();
+    }
+
+    public String getName(){
+        return name;
+    }
+    TreeNode getNode(String st){
+        for (TreeNode tn: pointers){
+            if(tn.getName().equals(st)){
+                return tn;
+            }
+        }
+        return null;
+    }
+}
+
 public class Seven {
 
 
@@ -31,6 +66,33 @@ public class Seven {
                     ArrayList<String> arr = new ArrayList<String>();
                     arr.add(num);
                     arr.add(d);
+                    arrl.add(arr);
+                }
+
+            }
+        }
+        catch(IOException e){
+            System.out.println("debug");
+        }
+        System.out.println(arrl);
+        return arrl;
+    }
+
+    ArrayList<ArrayList<String>> parse2(){
+        ArrayList<ArrayList<String>> arrl = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("inputseven.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] b = line.split("-> ");
+                String d = b[0].split(" ")[0];
+
+                String num = b[0].split("\\(")[1].split("\\)")[0];
+                if(b.length > 1) {
+}
+                else{
+                    ArrayList<String> arr = new ArrayList<String>();
+                    arr.add(num);
+                    arr.add(d);
                 }
             }
         }
@@ -39,6 +101,31 @@ public class Seven {
         }
         System.out.println(arrl);
         return arrl;
+    }
+
+    public static void parseTree(){
+        ArrayList<ArrayList<String>> arrl = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("inputseven.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] b = line.split("-> ");
+                String d = b[0].split(" ")[0];
+
+                String num = b[0].split("\\(")[1].split("\\)")[0];
+                if(b.length > 1) {
+
+                }
+                else{
+                    ArrayList<String> arr = new ArrayList<String>();
+                    arr.add(num);
+                    arr.add(d);
+                }
+            }
+        }
+        catch(IOException e){
+            System.out.println("debug");
+        }
+        System.out.println(arrl);
     }
 
     public static void main(String[] ar){
@@ -55,24 +142,35 @@ public class Seven {
 
                     }
                     else {
-                        System.out.println("debug " + get(arrl.get(x), arrlarrl) + ", " + get(arrl.get(y), arrlarrl));
+                        //System.out.println("debug " + get(arrl.get(x), arrlarrl) + ", " + get(arrl.get(y), arrlarrl));
                         allEquals = false;
+                        System.out.println(false);
                         break;
                     }
                 }
+
+            }
+            System.out.println(allEquals);
+            if(allEquals){
+                System.out.println("debug");
+                arrlarrl2.remove(arrl);
             }
         }
+        arrlarrl = deepCopy(arrlarrl2);
         System.out.println(arrlarrl2);
-        /**
-            arrlarrl = deepCopy(arrlarrl2);
-            for (ArrayList<String> strs : arrlarrl) {
-                for (int x = 2; x < strs.size(); x++) {
-                    System.out.println("looking for " + strs.get(x));
-                    remove(strs.get(x), arrlarrl, arrlarrl2);
+        for(ArrayList<String> arrl: arrlarrl){
+            for(int x = 2; x < arrl.size(); x++){
+                if (contains(arrl.get(x), arrlarrl)){
+                    System.out.println("AAAAAAAAA");
+                    arrlarrl2.remove(arrl);
+                    break;
                 }
             }
-            System.out.println(arrlarrl2);
-        **/
+        }
+System.out.println(arrlarrl2);
+        for(ArrayList<String> str : arrlarrl2){
+
+        }
     }
     public static void remove(String s, ArrayList<ArrayList<String>> arrlarrl, ArrayList<ArrayList<String>> arrlarrl2){
         for(ArrayList<String> strs : arrlarrl){
@@ -89,6 +187,7 @@ public class Seven {
                 return Integer.parseInt(strs.get(0));
             }
         }
+        System.out.println(s);
         return -1;
     }
 
@@ -104,5 +203,13 @@ public class Seven {
         }
         return deepCopy;
     }
-
+    public static boolean contains(String s, ArrayList<ArrayList<String>> arrlarrl){
+        for(ArrayList<String> strs : arrlarrl) {
+            if (strs.get(1).equals(s)) {
+                return true;
+            }
+        }
+        System.out.println(s);
+        return false;
+    }
 }
